0e64133eb160ad44223f4ca42a4ba2ffa8a21bda
Jatin Suthar <jatin.suthar.here@gmail.com>
2025-02-05 22:42:40 +0530

    added new postgresql db on render

--- constants/constants.py
+++ constants/constants.py
@@ -18,19 +18,19 @@ URL_PARAMS = {
 
 ### DataBase Configurations
 ### [Render's PostgreSQL DB] 
-# HOSTNAME = "dpg-ctsdo4tumphs73flrbr0-a"
-# DATABASE = "crypto_currency_db_cb60"
-# USERNAME = "crypto_currency_db_cb60_user"
-# PASSWORD = "FJ9tobXuAcfpjoe3MpBShtz6zr1esXW9"
-# INTERNAL_DB_URL = "postgresql://crypto_currency_db_cb60_user:FJ9tobXuAcfpjoe3MpBShtz6zr1esXW9@dpg-ctsdo4tumphs73flrbr0-a/crypto_currency_db_cb60"
-# EXTERNAL_DB_URL = "postgresql://crypto_currency_db_cb60_user:FJ9tobXuAcfpjoe3MpBShtz6zr1esXW9@dpg-ctsdo4tumphs73flrbr0-a.oregon-postgres.render.com/crypto_currency_db_cb60"
-# PSQL_CMD = "PGPASSWORD=FJ9tobXuAcfpjoe3MpBShtz6zr1esXW9 psql -h dpg-ctsdo4tumphs73flrbr0-a.oregon-postgres.render.com -U crypto_currency_db_cb60_user crypto_currency_db_cb60"
+HOSTNAME = "dpg-cuhpjsqn91rc73c6bkn0-a"
+DATABASE = "cryptocurrencyprodb_2"
+USERNAME = "cryptocurrencyprodb_2_user"
+PASSWORD = "x2Nbj6K0YousQhsMr89uCAKWjrRdyx0q"
+INTERNAL_DB_URL = "postgresql://cryptocurrencyprodb_2_user:x2Nbj6K0YousQhsMr89uCAKWjrRdyx0q@dpg-cuhpjsqn91rc73c6bkn0-a/cryptocurrencyprodb_2"
+EXTERNAL_DB_URL = "postgresql://cryptocurrencyprodb_2_user:x2Nbj6K0YousQhsMr89uCAKWjrRdyx0q@dpg-cuhpjsqn91rc73c6bkn0-a.oregon-postgres.render.com/cryptocurrencyprodb_2"
+PSQL_CMD = "PGPASSWORD=x2Nbj6K0YousQhsMr89uCAKWjrRdyx0q psql -h dpg-cuhpjsqn91rc73c6bkn0-a.oregon-postgres.render.com -U cryptocurrencyprodb_2_user cryptocurrencyprodb_2"
 
 
-### [Supabase's PostgreSQL DB]
-PSQL_CMD = "psql -h db.ocejkqvmwchksbeujgrk.supabase.co -p 5432 -d postgres -U postgres"
-PASSWORD = "J83027::11S"
-INTERNAL_DB_URL = f"postgresql://postgres:{PASSWORD}@db.ocejkqvmwchksbeujgrk.supabase.co:5432/postgres"
-EXTERNAL_DB_URL = f"postgresql://postgres:{PASSWORD}@db.ocejkqvmwchksbeujgrk.supabase.co:5432/postgres"
+# ### [Supabase's PostgreSQL DB]
+# PSQL_CMD = "psql -h db.ocejkqvmwchksbeujgrk.supabase.co -p 5432 -d postgres -U postgres"
+# PASSWORD = "J83027::11S"
+# INTERNAL_DB_URL = f"postgresql://postgres:{PASSWORD}@db.ocejkqvmwchksbeujgrk.supabase.co:5432/postgres"
+# EXTERNAL_DB_URL = f"postgresql://postgres:{PASSWORD}@db.ocejkqvmwchksbeujgrk.supabase.co:5432/postgres"
 
 
